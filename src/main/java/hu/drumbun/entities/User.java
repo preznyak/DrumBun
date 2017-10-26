5969af5a850bce6c57af7bbf46a8da5cb7589b90
Preznyák <laszlopreznyak11@gmail.com>
2017-10-26 14:10:13 +0300

    User entity added

---
+++ src/main/java/hu/drumbun/entities/User.java
@@ -0,0 +1,62 @@
+package hu.drumbun.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @GeneratedValue
+    @Id
+    private long id;
+
+    private String email;
+    
+    private String password;
+
+    private String oauth_provider;
+
+    private String oauth_uid;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getOauth_provider() {
+        return oauth_provider;
+    }
+
+    public void setOauth_provider(String oauth_provider) {
+        this.oauth_provider = oauth_provider;
+    }
+
+    public String getOauth_uid() {
+        return oauth_uid;
+    }
+
+    public void setOauth_uid(String oauth_uid) {
+        this.oauth_uid = oauth_uid;
+    }
+}
+
