711ce8445eff6a61d474c2479b8f04a51bce2343
Preznyák László <laszlopreznyak11@gmail.com>
2017-11-16 14:48:20 +0200

    UserProfileServiceImpl created

---
+++ src/main/java/hu/drumbun/service/userprofile/impl/UserProfileServiceImpl.java
@@ -0,0 +1,61 @@
+package hu.drumbun.service.userprofile.impl;
+
+import hu.drumbun.controller.userprofile.model.UserProfileResponse;
+import hu.drumbun.enums.DriverLicense;
+import hu.drumbun.enums.Gender;
+import hu.drumbun.repository.userprofile.UserProfileRepository;
+import hu.drumbun.service.userprofile.UserProfileService;
+import hu.drumbun.service.userprofile.converter.CreateUserProfileRequestConverter;
+import hu.drumbun.service.userprofile.converter.UpdateUserProfileRequestConverter;
+import hu.drumbun.service.userprofile.converter.UserProfileResponseConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Qualifier("userProfileService")
+public class UserProfileServiceImpl implements UserProfileService{
+
+    private final UserProfileRepository userProfileRepository;
+    private final CreateUserProfileRequestConverter createUserProfileRequestConverter;
+    private final UpdateUserProfileRequestConverter updateUserProfileRequestConverter;
+    private final UserProfileResponseConverter userProfileResponseConverter;
+
+    @Autowired
+    public UserProfileServiceImpl(UserProfileRepository userProfileRepository, CreateUserProfileRequestConverter createUserProfileRequestConverter, UpdateUserProfileRequestConverter updateUserProfileRequestConverter, UserProfileResponseConverter userProfileResponseConverter) {
+        this.userProfileRepository = userProfileRepository;
+        this.createUserProfileRequestConverter = createUserProfileRequestConverter;
+        this.updateUserProfileRequestConverter = updateUserProfileRequestConverter;
+        this.userProfileResponseConverter = userProfileResponseConverter;
+    }
+
+    @Override
+    public List<UserProfileResponse> findAll() {
+        return userProfileRepository.findAll().stream()
+                .map(userProfileResponseConverter::from)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public UserProfileResponse findUserProfileByUserId(long id) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public List<UserProfileResponse> findAllByDriverLicense(DriverLicense driverLicense) {
+        return userProfileRepository.findAllByDriverLicense(driverLicense).stream()
+                .map(userProfileResponseConverter::from)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<UserProfileResponse> findAllByGender(Gender gender) {
+        return userProfileRepository.findAllByGender(gender).stream()
+                .map(userProfileResponseConverter::from)
+                .collect(Collectors.toList());
+    }
+}
