48231cf363b33066de9eff003a68a1c9ee603d6e
Preznyák László <laszlopreznyak11@gmail.com>
2017-11-16 20:12:25 +0200

    NeedModel created

---
+++ src/main/java/hu/drumbun/controller/need/NeedModel.java
@@ -0,0 +1,28 @@
+package hu.drumbun.controller.need;
+
+import hu.drumbun.entities.Path;
+
+public class NeedModel {
+
+    private Path path;
+    private String comment;
+
+    public NeedModel() {
+    }
+
+    public Path getPath() {
+        return path;
+    }
+
+    public void setPath(Path path) {
+        this.path = path;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
