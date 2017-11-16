54ce64ae47c3955c0d0f51d5e5f3f8fbdefd9073
Preznyák László <laszlopreznyak11@gmail.com>
2017-11-16 19:03:10 +0200

    PathRepository created

---
+++ src/main/java/hu/drumbun/repository/path/PathRepository.java
@@ -0,0 +1,10 @@
+package hu.drumbun.repository.path;
+
+import hu.drumbun.entities.Path;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PathRepository extends JpaRepository<Path, Long>{
+
+}
