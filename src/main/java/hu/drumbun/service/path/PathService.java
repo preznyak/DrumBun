5d80af20f9ee8f24a3bc8e028365b2dd710eb3a7
Preznyák László <laszlopreznyak11@gmail.com>
2017-11-16 19:09:56 +0200

    PathService created

---
+++ src/main/java/hu/drumbun/service/path/PathService.java
@@ -0,0 +1,12 @@
+package hu.drumbun.service.path;
+
+import hu.drumbun.entities.Path;
+
+import java.util.List;
+
+public interface PathService {
+    List<Path> findAll();
+    Path findById(long id);
+    Path findByStart(String start);
+    Path findByDestination(String destination);
+}
---
+++ src/main/java/hu/drumbun/service/path/impl/PathServiceImpl.java
@@ -0,0 +1,41 @@
+package hu.drumbun.service.path.impl;
+
+import hu.drumbun.entities.Path;
+import hu.drumbun.repository.path.PathRepository;
+import hu.drumbun.service.path.PathService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PathServiceImpl implements PathService {
+
+    private final PathRepository pathRepository;
+
+    @Autowired
+    public PathServiceImpl(PathRepository pathRepository) {
+        this.pathRepository = pathRepository;
+    }
+
+    @Override
+    public List<Path> findAll() {
+        return pathRepository.findAll();
+    }
+
+    @Override
+    public Path findById(long id) {
+        return pathRepository.findOne(id);
+    }
+
+    @Override
+    public Path findByStart(String start) {
+        return pathRepository.findByStart(start);
+    }
+
+    @Override
+    public Path findByDestination(String destination) {
+        return pathRepository.findByDestination(destination);
+    }
+}
