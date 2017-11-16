1a1e846321cdd062b0a55a58bcb6a1120864fd96
Preznyák László <laszlopreznyak11@gmail.com>
2017-11-16 14:51:33 +0200

    UserProfileController Initial

--- src/main/java/hu/drumbun/controller/userprofile/UserProfileController.java
+++ src/main/java/hu/drumbun/controller/userprofile/UserProfileController.java
@@ -1,4 +1,26 @@
 package hu.drumbun.controller.userprofile;
 
+import hu.drumbun.controller.userprofile.model.UserProfileResponse;
+import hu.drumbun.service.userprofile.UserProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/userprofile")
 public class UserProfileController {
+
+    @Autowired
+    @Qualifier("userProfileService")
+    UserProfileService userProfileService;
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<UserProfileResponse> getAllUsers() {
+        return userProfileService.findAll();
+    }
 }
