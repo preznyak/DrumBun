158e00259262176986fd963d3a5116d88bf745e1
Preznyák László <laszlopreznyak11@gmail.com>
2017-11-16 14:08:44 +0200

    UserProfileResponse class created

---
+++ src/main/java/hu/drumbun/controller/userprofile/model/UserProfileResponse.java
@@ -0,0 +1,50 @@
+package hu.drumbun.controller.userprofile.model;
+
+import hu.drumbun.entities.User;
+import hu.drumbun.enums.DriverLicense;
+import hu.drumbun.enums.Gender;
+
+import java.util.Date;
+
+public class UserProfileResponse {
+
+    private User user;
+    private Date birthDate;
+    private Gender gender;
+    private DriverLicense driverLicense;
+
+    public UserProfileResponse() {
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public DriverLicense getDriverLicense() {
+        return driverLicense;
+    }
+
+    public void setDriverLicense(DriverLicense driverLicense) {
+        this.driverLicense = driverLicense;
+    }
+}
