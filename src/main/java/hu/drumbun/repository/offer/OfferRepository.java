f65af0bd39c880947696a3b80d42eccc1a06e6ae
Preznyák László <laszlopreznyak11@gmail.com>
2017-11-16 20:53:02 +0200

    NeedRepository created

---
+++ src/main/java/hu/drumbun/repository/offer/OfferRepository.java
@@ -0,0 +1,13 @@
+package hu.drumbun.repository.offer;
+
+import hu.drumbun.entities.Offer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OfferRepository extends JpaRepository<Offer, Long>{
+    List<Offer> findAll();
+
+}
