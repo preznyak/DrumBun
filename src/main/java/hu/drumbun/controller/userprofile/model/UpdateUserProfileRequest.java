d65c0ee68e9268d257dda63f1c5a62be19ec1077
Preznyák László <laszlopreznyak11@gmail.com>
2017-11-16 14:08:29 +0200

    UpdateUserProfileRequest class created

---
+++ src/main/java/hu/drumbun/controller/userprofile/model/UpdateUserProfileRequest.java
@@ -0,0 +1,51 @@
+package hu.drumbun.controller.userprofile.model;
+
+import hu.drumbun.entities.User;
+import hu.drumbun.enums.DriverLicense;
+import hu.drumbun.enums.Gender;
+
+import java.util.Date;
+
+public class UpdateUserProfileRequest {
+
+    private long id;
+    private User user;
+    private Date birthDate;
+    private Gender gender;
+    private DriverLicense driverLicense;
+
+    public UpdateUserProfileRequest() {
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public DriverLicense getDriverLicense() {
+        return driverLicense;
+    }
+
+    public void setDriverLicense(DriverLicense driverLicense) {
+        this.driverLicense = driverLicense;
+    }
+}
