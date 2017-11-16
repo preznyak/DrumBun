8718e4abc163e6f6d9c1a665070cc9398bd27bc1
Preznyák László <laszlopreznyak11@gmail.com>
2017-11-16 19:20:33 +0200

    PathRepository filled with methods

--- src/main/java/hu/drumbun/repository/path/PathRepository.java
+++ src/main/java/hu/drumbun/repository/path/PathRepository.java
@@ -4,7 +4,13 @@ import hu.drumbun.entities.Path;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PathRepository extends JpaRepository<Path, Long>{
+
+    List<Path> findAll();
+    Path findByStart(String start);
+    Path findByDestination(String destination);
 
 }
