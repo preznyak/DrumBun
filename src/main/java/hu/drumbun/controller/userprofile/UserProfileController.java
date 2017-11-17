7d9d28c3d8bc306e455bc98365625f400be8f108
Preznyák László <laszlopreznyak11@gmail.com>
2017-11-17 17:26:45 +0200

    CRUD methods in controller

--- src/main/java/hu/drumbun/controller/userprofile/UserProfileController.java
+++ src/main/java/hu/drumbun/controller/userprofile/UserProfileController.java
@@ -1,14 +1,17 @@
 package hu.drumbun.controller.userprofile;
 
+import hu.drumbun.controller.userprofile.model.CreateUserProfileRequest;
+import hu.drumbun.controller.userprofile.model.UpdateUserProfileRequest;
 import hu.drumbun.controller.userprofile.model.UserProfileResponse;
+import hu.drumbun.enums.DriverLicense;
+import hu.drumbun.enums.Gender;
 import hu.drumbun.service.userprofile.UserProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.print.attribute.standard.MediaPrintableArea;
 import java.util.List;
 
 @RestController
@@ -22,5 +25,31 @@ public class UserProfileController {
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<UserProfileResponse> getAllUsers() {
         return userProfileService.findAll();
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE, value = "/{driverLicense}")
+    public List<UserProfileResponse> findAllByDriverLicense(@PathVariable DriverLicense driverLicense){
+        return userProfileService.findAllByDriverLicense(driverLicense);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE, value = "/{gender}")
+    public List<UserProfileResponse> findAllByGender(@PathVariable Gender gender){
+        return userProfileService.findAllByGender(gender);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, value = "/create")
+    public void createUserProfile(@RequestBody CreateUserProfileRequest createUserProfileRequest){
+        userProfileService.createUserProfile(createUserProfileRequest);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, value = "/update")
+    public void updateUserProfile(@RequestBody UpdateUserProfileRequest updateUserProfileRequest){
+        userProfileService.updateUserProfile(updateUserProfileRequest);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/delete/{id}")
+    public void deleteUserProfileById(@PathVariable long id){
+        userProfileService.deleteUserProfileById(id);
     }
 }
