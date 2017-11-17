6268b5bb2d95824b10620198631e0644c48f6fb4
Preznyák László <laszlopreznyak11@gmail.com>
2017-11-17 18:58:36 +0200

    new methods in repositories

--- src/main/java/hu/drumbun/repository/need/NeedRepository.java
+++ src/main/java/hu/drumbun/repository/need/NeedRepository.java
@@ -4,9 +4,14 @@ import hu.drumbun.entities.Need;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface NeedRepository extends JpaRepository<Need, Long>{
     List<Need> findAll();
+    List<Need> findByPath_Destination(String destination);
+    List<Need> findByPath_Start(String start);
+    List<Need> findByDateAfter(Date date);
+    List<Need> findByDateBefore(Date date);
 }
