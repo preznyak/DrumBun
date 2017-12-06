af4ed1c213278bd08e6d3447a58ecafa8dd30e73
Preznyák László <laszlopreznyak11@gmail.com>
2017-12-06 15:35:57 +0200

    Need search works fine :D

--- src/main/java/hu/drumbun/service/need/impl/NeedServiceImpl.java
+++ src/main/java/hu/drumbun/service/need/impl/NeedServiceImpl.java
@@ -3,14 +3,19 @@ package hu.drumbun.service.need.impl;
 import hu.drumbun.controller.need.model.NeedModel;
 import hu.drumbun.entities.Need;
 import hu.drumbun.entities.Offer;
+import hu.drumbun.entities.User;
 import hu.drumbun.repository.need.NeedRepository;
 import hu.drumbun.repository.offer.OfferRepository;
 import hu.drumbun.repository.path.PathRepository;
+import hu.drumbun.repository.user.UserRepository;
 import hu.drumbun.service.need.NeedService;
 import hu.drumbun.service.need.converter.NeedModelConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,13 +27,15 @@ public class NeedServiceImpl implements NeedService{
     private final PathRepository pathRepository;
     private final OfferRepository offerRepository;
     private final NeedModelConverter needModelConverter;
+    private final UserRepository userRepository;
 
     @Autowired
-    public NeedServiceImpl(NeedRepository needRepository, PathRepository pathRepository, OfferRepository offerRepository, NeedModelConverter needModelConverter) {
+    public NeedServiceImpl(NeedRepository needRepository, PathRepository pathRepository, OfferRepository offerRepository, NeedModelConverter needModelConverter, UserRepository userRepository) {
         this.needRepository = needRepository;
         this.pathRepository = pathRepository;
         this.offerRepository = offerRepository;
         this.needModelConverter = needModelConverter;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -87,13 +94,31 @@ public class NeedServiceImpl implements NeedService{
     }
 
     @Override
-    public void receiveOffer(long offerId, long needId) {
-        Offer offer = offerRepository.findOne(offerId);
+    public void receiveOffer(long needId, String username) {
         Need need = needRepository.findOne(needId);
+        User user = userRepository.findByUsername(username);
+        Offer newOffer = new Offer();
+        newOffer.setUser(user);
+        newOffer.setDate(need.getDate());
+        newOffer.setPath(need.getPath());
+        newOffer.setMaxSeats(3);
         List<Offer> offers = need.getOffers();
-        offers.add(offer);
-        need.setUser(offer.getUser());
+        offers.add(newOffer);
         need.setOffers(offers);
         needRepository.save(need);
+    }
+
+    @Override
+    public List<NeedModel> findByAll(String start, String destination, String date) {
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm");
+        Date date1 = null;
+        try {
+            date1 = formatter.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return needRepository.findByPath_StartAndPath_DestinationAndDateAfter(start,destination,date1).stream()
+                .map(needModelConverter::fromNeedtoNeedModel)
+                .collect(Collectors.toList());
     }
 }
